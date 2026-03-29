cdfaaa81b8556ddf88fa7a9a22c7bb6e8bae9796
armankhan125 <sardararman.1267@gmail.com>
2026-03-29 19:52:06 +0500

    Updated index page design and fixed invisible character errors in Django template

--- jusa_chacha/urls.py
+++ jusa_chacha/urls.py
@@ -6,20 +6,19 @@ from django.conf.urls.static import static
 from shop.views import product_list, product_detail, about 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    
-    # Home Page: Saare products nazar aayenge
-    path('', product_list, name='product_list'),
-    
-    # Product detail page: Jab koi kisi shirt par click karega
-    path('product/<int:pk>/', product_detail, name='product_detail'), 
-    
-    # About Us aur Size Guide page
-    path('about/', about, name='about'), 
-    
+    path('admin/', admin.site.urls),
+    
+    # Home Page: Saare products nazar aayenge
+    path('', product_list, name='product_list'),
+    
+    # Product detail page: Jab koi kisi shirt par click karega
+    path('product/<int:pk>/', product_detail, name='product_detail'), 
+    
+    # About Us aur Size Guide page
+    path('about/', about, name='about'), 
 ]
 
 # Media aur Static files ki setting (Images aur CSS ke liye)
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
